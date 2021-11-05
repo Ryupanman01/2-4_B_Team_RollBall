b4ecb5cdde55df63e5fa04cf1ccf5509c75c5e38
KBC12006160\kbc12006160 <ryu-ookouchi_20h@stu.kbc.ac.jp>
2021-11-05 09:01:07 +0900

    SE追加

---
+++ 2-4_B_team/Assets/Script/SE.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SE : MonoBehaviour
+{
+    AudioSource audioSource;
+
+
+    public AudioClip sound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+}
