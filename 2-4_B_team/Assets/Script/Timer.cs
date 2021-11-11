b71defce25e63edfdabf6e4e7aad2204c98b309e
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2021-11-11 10:30:50 +0900

    変更

--- 2-4_B_team/Assets/Script/Timer.cs
+++ 2-4_B_team/Assets/Script/Timer.cs
@@ -11,15 +11,20 @@ public class Timer : MonoBehaviour
     float countdown = 4.0f;
     int count;
 
+    public AudioClip sound1;     //カウントダウンのSE
+    AudioSource audioSource;    //カウントダウンのSE
+
+
     private void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();  //SE
     }
 
     private void Update()
     {
+
         //1秒以上なら
-        if(countdown >= 1)
+        if (countdown >= 1)
         {
             Application.targetFrameRate = 60;   //60Fpsにする
             Debug.Log("カウントダウンのFPS：" + Application.targetFrameRate);
@@ -27,6 +32,13 @@ public class Timer : MonoBehaviour
             countdown -= Time.deltaTime;
             count = (int)countdown;
             CountDownText.text = count.ToString();
+
+            if (CountDownText.enabled)  //一回だけ音を鳴らす
+            {
+                audioSource.PlayOneShot(sound1);//SEを鳴らす
+            }
+
+
             StartText.enabled = false;      //スタートテキストを非表示にする
         }
         //1秒より少ないなら
@@ -35,7 +47,8 @@ public class Timer : MonoBehaviour
             Application.targetFrameRate = 30;   //30Fpsにする
             Debug.Log("スタートのFPS：" + Application.targetFrameRate);
             countdown -= Time.deltaTime;
-            CountDownText.text = "";        //カウントダウンテキストを無にする
+            //CountDownText.text = "";        //カウントダウンテキストを無にする
+            CountDownText.enabled = false;
             StartText.enabled = true;       //スタートテキストを表示する
         }
         //0秒よりすくないなら
