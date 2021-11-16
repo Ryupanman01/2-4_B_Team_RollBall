2d9c43b1e1185229005ec95aaae5a350c085762b
KBC12006201\kbc12006201 <hayato-uchima-20h@stu.kbc.ac.jp>
2021-11-16 14:47:44 +0900

    爆弾途中まで制作済

---
+++ 2-4_B_team/Assets/Script/StopTrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopTrap : MonoBehaviour
+{
+    public GameObject effectPrefab;
+    private UnityStandardAssets.Vehicles.Ball.Ball ball;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // ポイント
+            // Ballの動きを司るスクリプトをオフにする。
+            ball = collision.gameObject.GetComponent<UnityStandardAssets.Vehicles.Ball.Ball>();
+
+            GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+            Destroy(effect, 2.0f);
+
+            // ポイント
+            // ２秒後にボールが動けるようにする。
+            Invoke("StopOff", 2.0f);
+        }
+    }
+
+    void StopOff()
+    {
+        // ポイント
+        // Ballの動きを司るスクリプトをオンにする。
+    }
+}
