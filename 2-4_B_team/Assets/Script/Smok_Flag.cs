2a099de932eb68dd138666df318472a9aab6fffd
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2021-11-24 14:20:12 +0900

    ステージの変更、開始時の土煙でないスクリプト

---
+++ 2-4_B_team/Assets/Script/Smok_Flag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smok_Flag : MonoBehaviour
+{
+    private ParticleSystem ps;
+    public bool moduleEnabled;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+
+        ParticleSystem.EmissionModule emission = ps.emission;
+        moduleEnabled = false;
+        emission.enabled = moduleEnabled;
+
+        Invoke("Call", 4.5f);
+    }
+    void Call()
+    {
+        ParticleSystem.EmissionModule emission = ps.emission;
+        if (moduleEnabled)
+        {
+            moduleEnabled = false;
+        }
+        else
+        {
+            moduleEnabled = true;
+        }
+        emission.enabled = moduleEnabled;
+    }
+}
