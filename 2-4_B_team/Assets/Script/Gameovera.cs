3f199e9506d889725399d2e45bb119f5636a428d
KBC12006034\kbc12006034 <moriki-oota-20h@stu.kbc.ac.jp>
2021-11-24 14:15:26 +0900

    落ちた時のリスタート

---
+++ 2-4_B_team/Assets/Script/Gameovera.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Gameovera : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "GameOver")
+        {
+            //Destroy(this.gameObject);
+            //gameOverText.text = "GameOver";
+            this.transform.position = new Vector3(0, 1.3f, 0);
+        }
+    }
+}
